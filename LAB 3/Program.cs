b171ed1ac476769d41b141fe445873ec193adce8
OlegPazny <petr.jakimovets@gmail.com>
2021-11-16 13:57:09 +0300

    Lab 3

---
+++ LAB 3/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Time t1 = new Time(03, 15, 36);
+            Time t2 = new Time(05, 36, 55);
+            Time t3 = new Time(08, 15, 06);
+            Time t4 = new Time(11, 08, 10);
+            Time t5 = new Time(12, 09, 54);
+            Time t6 = new Time(15, 10, 41);
+            Time t7 = new Time(20, 21, 20);
+            Time t8 = new Time(23, 22, 22);
+            Time[] mass = new Time[8];
+            mass[0] = t1;
+            mass[1] = t2;
+            mass[2] = t3;
+            mass[3] = t4;
+            mass[4] = t5;
+            mass[5] = t6;
+            mass[6] = t7;
+            mass[7] = t8;
+            Time s1 = Time.GetInstance();
+            Time s2 = Time.GetInstance();
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Time works, both variables contain the same instance.");
+            }
+            else
+            {
+                Console.WriteLine("Time failed, variables contain different instances.");
+            }
+            double sec = t1.GetSeconds();
+            t1.ReturnSec(ref sec);
+            t1.ReturnMin(out double minutes);
+            //вывод времени с заданным числом часов
+            for (int i = 0; i < mass.Length; i++)
+            {
+                if (mass[i].hours == 15)
+                    Console.WriteLine($"Вывод времени с заданным числом часов: {mass[i]}");
+            }
+            //ночь 0-6, утро 7-11, день 12-16, вечер 17-24
+            for (int i=0;i<mass.Length; i++)
+            {
+                if ((mass[i].hours > 0) && (mass[i].hours <= 6))
+                    Console.WriteLine($"Ночное время: { mass[i]}");
+                if ((mass[i].hours > 7) && (mass[i].hours <= 11))
+                    Console.WriteLine($"Утреннее время: { mass[i]}");
+                if ((mass[i].hours > 12) && (mass[i].hours <= 16))
+                    Console.WriteLine($"Дневное время: { mass[i]}");
+                if ((mass[i].hours > 17) && (mass[i].hours <= 24))
+                    Console.WriteLine($"Вечернее время: { mass[i]}");
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LAB 3/Time.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace LAB_3
+{
+    public class Time
+    {
+        public byte hours;
+        private byte minutes;
+        private byte seconds;
+        private readonly Guid id;//поле только для чтения
+        private const int year = 2021;
+        static public int i = 0;
+
+        public Time(byte hours, byte minutes, byte seconds)
+        {
+            this.id = Guid.NewGuid();
+            if ((hours > 23 || hours < 0) || (minutes > 59 || minutes < 0) || (seconds > 59 || seconds < 0)) 
+            {
+                Console.WriteLine("error"); //генерация исключения
+            }
+            this.hours = hours;
+            this.minutes = minutes;
+            this.seconds = seconds;
+            i++;
+        }
+        /*private Time(): this(0, 0, 0)
+        {
+            i++;
+        } //конструктор с перенаправлением на конструктор с 3-мя параметрами*/
+        public Time(byte hours=2, byte minutes=10)
+        {
+            this.id = Guid.NewGuid();
+            seconds = 0;
+            if ((hours > 23 || hours < 0) || (minutes > 59 || minutes < 0))
+            {
+                Console.WriteLine("error");
+            }
+            this.hours = hours;
+            this.minutes = minutes;
+            i++;
+        }
+        private Time()
+        {
+            this.id = Guid.NewGuid();//копирнуть во все констр
+            i++;
+        }
+
+        private static Time instance;
+
+
+        // Это статический метод, управляющий доступом к экземпляру одиночки.
+        // При первом запуске, он создаёт экземпляр одиночки и помещает его в
+        // статическое поле. При последующих запусках, он возвращает клиенту
+        // объект, хранящийся в статическом поле.
+        public static Time GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Time();
+            }
+            return instance;
+        }
+
+        public void SetHours(byte value)
+        {
+            if ((hours > 23) || (hours < 0))
+            {
+                Console.WriteLine("error");
+            }
+            hours = value;
+        }
+        public double GetHours()
+        {
+            return hours;
+        }
+
+        public void SetMinutes(byte value)
+        {
+            if ((minutes > 59) || (minutes < 0))
+            {
+                Console.WriteLine("error");
+            }
+            minutes = value;
+        }
+        public double GetMinutes()
+        {
+            return minutes;
+        }
+
+        public void SetSeconds(byte value)
+        {
+            if ((seconds > 59) || (seconds < 0))
+            {
+                Console.WriteLine("error");
+            }
+            seconds = value;
+        }
+        public double GetSeconds()
+        {
+            return seconds;
+        }
+        //статический конструктор
+        static Time()
+        {
+            i = 0;
+            Console.WriteLine("Время: \n");
+        }
+        public void ReturnSec(ref double seconds)
+        {
+            seconds += 1;
+        }
+        public void ReturnMin(out double minutes)
+        {
+            minutes=this.minutes;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}:{1}:{2}", hours, minutes, seconds);
+        }
+        public static void SingletonInfo()
+        {
+            Console.WriteLine("SINGLETON {0}:{1}:{2}", instance.hours, instance.minutes, instance.seconds);
+        }
+        public override int GetHashCode()
+        {
+            return this.minutes.GetHashCode();
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj.GetType() != this.GetType()) return false;
+
+            Time t = (Time)obj;
+            return (this.hours == t.hours);
+        }
+    }
+}
